194f7b821f7b88c8c351aa52eda3a9da1eb2bc7a
basedslime <jaydenhaisley101@gmail.com>
2025-12-03 19:07:58 -0500

    added sketching and submitting numbers

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -39,6 +39,12 @@ class Cell:
             text = font.render(str(self.value), True, (0, 0, 0))
             text_rect = text.get_rect(center = (x+cellsize//2, y+cellsize//2))
             self.screen.blit(text, text_rect)
+        elif self.sketched_value != 0:
+            font = pygame.font.SysFont('comicsans', 30)
+            text = font.render(str(self.sketched_value), True, (150, 150, 150))
+            text_rect = text.get_rect(center = (x+cellsize//2, y+cellsize//2))
+            self.screen.blit(text, text_rect)
+
 
 
 """
@@ -484,24 +490,31 @@ if __name__ == "__main__":
                     board.draw()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_1:
-                    board.place_number(1)
+                    board.sketch(1)
                 if event.key == pygame.K_2:
-                    board.place_number(2)
+                    board.sketch(2)
                 if event.key == pygame.K_3:
-                    board.place_number(3)
+                    board.sketch(3)
                 if event.key == pygame.K_4:
-                    board.place_number(4)
+                    board.sketch(4)
                 if event.key == pygame.K_5:
-                    board.place_number(5)
+                    board.sketch(5)
                 if event.key == pygame.K_6:
-                    board.place_number(6)
+                    board.sketch(6)
                 if event.key == pygame.K_7:
-                    board.place_number(7)
+                    board.sketch(7)
                 if event.key == pygame.K_8:
-                    board.place_number(8)
+                    board.sketch(8)
                 if event.key == pygame.K_9:
-                    board.place_number(9)
+                    board.sketch(9)
                 if event.key == pygame.K_BACKSPACE:
                     board.clear()
+                if event.key == pygame.K_RETURN:
+                    if board.selected_row is not None and board.selected_col is not None:
+                        cell = board.sudoku_board[board.selected_row][board.selected_col]
+                        if cell.changeable and cell.sketched_value != 0:
+                            board.place_number(cell.sketched_value)
+                            cell.changeable = False
+
         pygame.display.update()
     pygame.quit()
