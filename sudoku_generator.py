aedead205e3fc2fe4b898ed035d28f26ff8bd2b2
basedslime <jaydenhaisley101@gmail.com>
2025-12-03 20:08:36 -0500

    added back sketching and submitting

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -560,25 +560,31 @@ if __name__ == "__main__":
 
             if event.type == pygame.KEYDOWN and game_state == "game":
                 if event.key == pygame.K_1:
-                    board.place_number(1)
+                    board.sketch(1)
                 if event.key == pygame.K_2:
-                    board.place_number(2)
+                    board.sketch(2)
                 if event.key == pygame.K_3:
-                    board.place_number(3)
+                    board.sketch(3)
                 if event.key == pygame.K_4:
-                    board.place_number(4)
+                    board.sketch(4)
                 if event.key == pygame.K_5:
-                    board.place_number(5)
+                    board.sketch(5)
                 if event.key == pygame.K_6:
-                    board.place_number(6)
+                    board.sketch(6)
                 if event.key == pygame.K_7:
-                    board.place_number(7)
+                    board.sketch(7)
                 if event.key == pygame.K_8:
-                    board.place_number(8)
+                    board.sketch(8)
                 if event.key == pygame.K_9:
-                    board.place_number(9)
+                    board.sketch(9)
                 if event.key == pygame.K_BACKSPACE:
                     board.clear()
+                if event.key == pygame.K_RETURN:
+                    if board.selected_row is not None and board.selected_col is not None:
+                        cell = board.sudoku_board[board.selected_row][board.selected_col]
+                        if cell.changeable and cell.sketched_value != 0:
+                            board.place_number(cell.sketched_value)
+                            cell.changeable = False
 
                 if board.is_full():
                     if board.check_board():
